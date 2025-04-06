271c5246c7995e8160542ff23d5728f1993d2a31
Carlos Pereira Dias <carlospdiase@gmail.com>
2025-04-06 08:57:25 -0300

    :fire: Exclui código de apoio

--- thymeleaf/src/main/java/br/com/cpdias/sb3/config/AppInfo.java
+++ thymeleaf/src/main/java/br/com/cpdias/sb3/config/AppInfo.java
@@ -42,24 +42,4 @@ public class AppInfo {
         return applicationName;
     }
 
-    public static void main(String[] args) {
-        Instant instant = Instant.now();
-        System.out.println(instant.toString());
-
-        LocalDateTime localDateTime = instant.atZone(ZoneId.systemDefault()).toLocalDateTime();
-
-        System.out.println(localDateTime.toString());
-    }
 }
-
-/*
-https://chatgpt.com/c/67f1d115-e434-800f-853f-cbde811f9637
-
-build.artifact=spring-boot3-thymeleaf-basic-demo
-build.description=A basic demo project that shows how to use Thymeleaf with Spring Boot 3
-build.group=dev.andbin
-build.java.version=17
-build.name=Spring Boot 3 - Thymeleaf Basic Demo
-build.time=2025-04-06T01\:08\:59.875Z
-build.version=0.0.1
- */
--- thymeleaf/src/main/java/br/com/cpdias/sb3/config/LocalizationConfig.java
+++ thymeleaf/src/main/java/br/com/cpdias/sb3/config/LocalizationConfig.java
@@ -1,5 +1,6 @@
 package br.com.cpdias.sb3.config;
 
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
